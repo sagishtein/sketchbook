369883b7af59ceacc8764c86afc7f6baa88ebfa7
sagishtein <67749675+sagishtein@users.noreply.github.com>
2021-08-13 14:29:43 -0400

    Add CircleParty code

---
+++ CircleParty/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(25);
+  stroke(255);
+  noFill();
+  background(0,0,0);
+  
+}
+
+function draw() {
+  var rad = random(10,100);
+  ellipse(mouseX, mouseY,rad, rad);
+}
+
+function mousePressed(){
+  var Colors = [color('brown'), color('chocolate'), color ('crimson'), color('darkgoldenrod'), color('gold')];
+  noStroke();
+  fill(Colors[int(random(0,4))]);
+}
+
+function mouseReleased(){
+  stroke(255);
+  noFill();
+}
+
